2a1b1385e579eb7ae24d90257397605159201d8e
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-08-18 19:34:10 +0000

    creados los modelos

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -10,10 +10,68 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    post: Mapped["Post"] = relationship(back_populates="author")
+    comments: Mapped["Comment"] = relationship(back_populates="author")
+    likes: Mapped["Like"] = relationship(back_populates="author")
+
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    post_type: Mapped[str] = mapped_column(String(120), nullable=False)
+    caption: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped["User"] = relationship(back_populates="posts")
+    
+    comments: Mapped["Comment"] = relationship(back_populates="post")
+    likes: Mapped["Like"] = relationship(back_populates="post")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.post_type,
+            "caption": self.caption,
+            "url": self.url
+        }
+    
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+    author: Mapped["User"] = relationship(back_populates="posts")
+    post: Mapped["Post"] = relationship(back_populates="posts")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "user_id": self.user_id,
+            "post_id": self.post_id
+        }
+
+class Like(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+    author: Mapped["User"] = relationship(back_populates="posts")
+    post: Mapped["Post"] = relationship(back_populates="posts")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id
         }
